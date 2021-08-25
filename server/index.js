01105a9ff46761ed998cff9e93c32603fa3e998c
yeeun62 <byebye62@naver.com>
2021-08-25 13:32:36 +0900

    컨트롤러 파일 작성 진행중입니다.

--- server/index.js
+++ server/index.js
@@ -1,33 +1,36 @@
-require("dotenv").config();
-const cors = require("cors");
-const cookieParser = require("cookie-parser");
-const express = require("express");
+require('dotenv').config();
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+const express = require('express');
 const app = express();
 
-const controllers = require("./controllers");
-
-const port = process.env.PORT;
+const controllers = require('./controllers');
 
 app.use(express.json());
-app.use(cookieParser());
 app.use(express.urlencoded({ extended: false }));
 app.use(
-  cors({
-    origin: "https://", //! 도메인 url 적어야함.
-    credentials: true,
-    methods: ["GET", "POST", "PATCH", "DELETE", "OPTIONS"],
-  })
+    cors({
+        origin: ['https://'], //! 도메인 url 적어야함.
+        credentials: true,
+        methods: ['GET', 'POST', 'PATCH', 'DELETE', 'OPTIONS'],
+    }),
 );
 
-app.post("/login", controllers.Login);
-app.get("/signout", controllers.Signout);
-app.post("/signup", controllers.Signup);
-app.get("/content", controllers.content);
+app.use(cookieParser());
+app.post('/login', controllers.login);
+app.get('/signout', controllers.signout);
+app.post('/signup', controllers.signup);
 
-app.get("/movies/:id");
+app.get('/content', controllers.content);
+app.get('/content/:id', controllers.content);
+app.post('/content', controllers.content);
+app.patch('/content:id', controllers.content);
+app.delete('/content:id', controllers.content);
 
-app.listen(port, () => {
-  console.log("server listen on 4000");
+const HTTPS_PORT = process.env.HTTPS_PORT || 4000;
+
+app.listen(HTTPS_PORT, () => {
+    console.log('server listen on 4000');
 });
 
 module.exports = app;
