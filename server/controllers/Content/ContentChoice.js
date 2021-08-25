7754d74c95a2e02329d1ea17291b99d3d4515502
yeeun62 <byebye62@naver.com>
2021-08-25 22:19:08 +0900

    [server]/controller 진짜 완성.

--- server/controllers/Content/ContentChoice.js
+++ server/controllers/Content/ContentChoice.js
@@ -1,4 +1,6 @@
 module.exports = {
+    // 찬성버튼을 눌렀을때 입니다.
     agree: (req, res) => {},
+    // 반대버튼을 눌렀을때 입니다.
     disagree: (req, res) => {},
 };
--- server/controllers/users/userinfo.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    // 내정보 입니다.
-    mypage: (req, res) => {},
-    // 내정보 수정입니다.
-    retouchMypage: (req, res) => {},
-};
