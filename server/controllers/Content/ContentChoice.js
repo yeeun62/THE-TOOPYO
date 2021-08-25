97832e488985e11dfe4e9c17f95b74de1e045992
yeeun62 <byebye62@naver.com>
2021-08-25 20:54:38 +0900

    [server]/controller 완료입니다.

---
+++ server/controllers/Content/ContentChoice.js
@@ -0,0 +1,4 @@
+module.exports = {
+    agree: (req, res) => {},
+    disagree: (req, res) => {},
+};
---
+++ server/controllers/Content/ContentList.js
@@ -0,0 +1,12 @@
+module.exports = {
+    // 모든 게시물 보기입니다.
+    allContent: (req, res) => {},
+    // 특정 게시물 보기입니다.
+    detailContent: (req, res) => {},
+    // 게시물 생성입니다.
+    createContent: (req, res) => {},
+    // 게시물 수정입니다.
+    retouchContent: (req, res) => {},
+    // 게시물 삭제입니다.
+    deleteContent: (req, res) => {},
+};
