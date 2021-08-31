0e262ab3bc96b2b7ba49587e04b4e2a3494132fe
yeeun62 <byebye62@naver.com>
2021-09-01 00:01:12 +0900

    [server]/token

--- server/controllers/Content/ContentChoice.js
+++ server/controllers/Content/ContentChoice.js
@@ -1,30 +1,33 @@
 const { user } = require('../../models');
 const { agree } = require('../../models');
 const { disagree } = require('../../models');
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
 
 module.exports = {
     // 찬성버튼을 눌렀을때 입니다.
     agree: async (req, res) => {
         try {
-            const findUser = await user.findOne({ where: { email: req.body.session } }); //! session
-            if (findUser) {
+            const accessToken = req.cookies.accessToken;
+            const userInfo = await jwt.verify(accessToken, process.env.ACCESS_SECRET);
+            if (userInfo) {
                 // 반대한 기록이 있는지 확인
                 const checkDisagree = await disagree.findOne({
-                    where: { userId: findUser.id, contentId: req.params.id },
+                    where: { userId: userInfo.id, contentId: req.params.id },
                 });
                 // 반대한 기록이 있다면 삭제
                 if (checkDisagree) {
-                    await disagree.destroy({ where: { userId: findUser.id, contentId: req.params.id } });
+                    await disagree.destroy({ where: { userId: userInfo.id, contentId: req.params.id } });
                 }
                 // 찬성한 기록이 있는지 확인
-                const checkAgree = await agree.findOne({ where: { userId: findUser.id, contentId: req.params.id } });
+                const checkAgree = await agree.findOne({ where: { userId: userInfo.id, contentId: req.params.id } });
                 // 찬성한 기록이 있다면 찬성기록 삭제
                 if (checkAgree) {
-                    await agree.destroy({ where: { userId: findUser.id, contentId: req.params.id } });
+                    await agree.destroy({ where: { userId: userInfo.id, contentId: req.params.id } });
                     res.status(200).json({ message: 'cancel agress' });
                     // 찬성한 기록이 없다면 찬성 추가
                 } else {
-                    await agree.create({ userId: findUser.id, contentId: req.params.id });
+                    await agree.create({ userId: userInfo.id, contentId: req.params.id });
                     res.status(200).json({ message: 'agree complete' });
                 }
             }
@@ -36,22 +39,23 @@ module.exports = {
     // 반대버튼을 눌렀을때 입니다.
     disagree: async (req, res) => {
         try {
-            const findUser = await user.findOne({ where: { email: req.body.session } }); //! session
-            if (findUser) {
+            const accessToken = req.cookies.accessToken;
+            const userInfo = await jwt.verify(accessToken, process.env.ACCESS_SECRET);
+            if (userInfo) {
                 const checkAgree = await agree.findOne({
-                    where: { userId: findUser.id, contentId: req.params.id },
+                    where: { userId: userInfo.id, contentId: req.params.id },
                 });
                 if (checkAgree) {
-                    await agree.destroy({ where: { userId: findUser.id, contentId: req.params.id } });
+                    await agree.destroy({ where: { userId: userInfo.id, contentId: req.params.id } });
                 }
                 const checkDisagree = await disagree.findOne({
-                    where: { userId: findUser.id, contentId: req.params.id },
+                    where: { userId: userInfo.id, contentId: req.params.id },
                 });
                 if (checkDisagree) {
-                    await disagree.destroy({ where: { userId: findUser.id, contentId: req.params.id } });
+                    await disagree.destroy({ where: { userId: userInfo.id, contentId: req.params.id } });
                     res.status(200).json({ message: 'cancel disagress' });
                 } else {
-                    await disagree.create({ userId: findUser.id, contentId: req.params.id });
+                    await disagree.create({ userId: userInfo.id, contentId: req.params.id });
                     res.status(200).json({ message: 'disagree complete' });
                 }
             }
--- server/index.js
+++ server/index.js
@@ -1,53 +1,23 @@
 require('dotenv').config();
+const https = require('https');
 const cors = require('cors');
-const session = require('express-session');
-// const https = require('https');
-// const cookieParser = require('cookie-parser');
+const cookieParser = require('cookie-parser');
 const express = require('express');
 const app = express();
 
 const controllers = require('./controllers');
-const { sequelize } = require('./models');
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(
-    cors({
-        origin: true,
-        // 'the-toopyo.com'
-        credentials: true,
-        methods: ['GET', 'POST', 'PATCH', 'DELETE', 'OPTIONS'],
-    }),
-);
-
-//app.use(cookieParser());
-app.use(
-    session({
-        secret: 'thetoopyo',
-        resave: false,
-        saveUninitialized: true,
-        cookie: {
-            domain: 'localhost',
-            path: '/',
-            maxAge: 24 * 6 * 60 * 10000,
-            sameSite: 'none',
-            httpOnly: true,
-            secure: true,
-        },
-    }),
-);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(
     cors({
         origin: true,
-        //['https://the-toopyo.com'],
+        //['http://localhost:3000', 'https://the-toopyo.com'],
         credentials: true,
         methods: ['GET', 'POST', 'PATCH', 'DELETE', 'OPTIONS'],
     }),
 );
-
-//app.use(cookieParser());
+app.use(cookieParser());
 
 app.post('/login', controllers.login);
 app.get('/signout', controllers.signOut);
